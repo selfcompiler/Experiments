bd57925421cae54bab088ab0e9eedefa0e137e6b
Rahul Kumar Singh <kumarahul120292@gmail.com>
2020-01-07 20:45:24 +0530

    Create OptionalExample.java

---
+++ Optional/OptionalExample.java
@@ -0,0 +1,48 @@
+package java8.OptionalExample;
+
+import com.sun.tools.internal.ws.wsdl.document.soap.SOAPUse;
+
+import java.util.Optional;
+
+public class OptionalExample {
+
+    public static void main(String[] args) {
+
+        Optional<String> text= Optional.of("BANGALORE");
+
+        String nonnull="nonnull VALUE";
+
+        String nullString=null;
+
+
+
+        System.out.printf(" "+text);
+
+        System.out.println(text.get());
+
+        System.out.println(String.valueOf(text.hashCode()));
+
+        System.out.println(" X= "+Optional.ofNullable(nonnull));
+
+        System.out.println(" Y = "+Optional.ofNullable(nullString));
+
+        System.out.println(" Z = "+Optional.of(nonnull).get());
+
+        System.out.println(" Z = "+Optional.of(nullString));
+    }
+
+
+}
+
+/*
+ Optional[BANGALORE]BANGALORE
+886496127
+ X= Optional[nonnull VALUE]
+ Y = Optional.empty
+ Z = nonnull VALUE
+Exception in thread "main" java.lang.NullPointerException
+	at java.util.Objects.requireNonNull(Objects.java:203)
+	at java.util.Optional.<init>(Optional.java:96)
+	at java.util.Optional.of(Optional.java:108)
+	at java8.OptionalExample.OptionalExample.main(OptionalExample.java:31)
+  */
